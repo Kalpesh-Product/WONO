efa8dc33123d460748411a8cf13dddc0aa504c2f
AiwinrajWono <wono.uiuxdev.intern03@gmail.com>
2024-08-30 15:59:12 +0530

    build check

--- frontend/src/pages/Dashboard-pages/DashboardBooking.js
+++ frontend/src/pages/Dashboard-pages/DashboardBooking.js
@@ -6,7 +6,7 @@ const DashboardBooking = () => {
   return (
     <div className='dashboard-booking-container'>
     <div className='dashboard-booking'>
-        <h2 style={{color:'black' , padding:'1rem'}}>Booking Engine</h2>
+        
       <img src={Booking} alt='booking' />
     </div>
     </div>
