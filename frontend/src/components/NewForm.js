4bc8153f817b6c25c65da97ec1cf51736fc98dcd
AiwinrajWono <wono.uiuxdev.intern03@gmail.com>
2024-09-12 13:20:02 +0530

    deleted frontend

--- frontend/src/components/NewForm.js
+++
@@ -1,7 +0,0 @@
-import { Widget } from '@typeform/embed-react'
-
-const NewForm = () => {
-  return <Widget id="VBGfKUza" style={{ width: '50%' }} className="my-form" />
-}
-
-export default NewForm
