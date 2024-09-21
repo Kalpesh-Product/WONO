e22c13c7e6579fbe267a473b3de4cf1d66f7276f
AiwinrajWono <wono.uiuxdev.intern03@gmail.com>
2024-09-21 10:14:03 +0530

    cors error generic fix

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,8 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 const { parse, format } = require('date-fns');
 
+app.use(cors());
+
 
 const app = express();
 const port = process.env.PORT;
@@ -37,12 +39,7 @@ app.use(session({
     maxAge: 1000 * 60 * 60 * 2 // 2 hours
   }
 }))
-app.use(cors({
-  origin: process.env.FRONTEND_PORT,
-  methods: ["GET", "POST"],
-  credentials: true
-}
-));
+
 
 // Setup Nodemailer transporter
 const transporter = nodemailer.createTransport({
