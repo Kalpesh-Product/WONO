2a4db1e46c8a9b5de857794382f7815e1cd02216
AllanMScode <97829785+AllanMScode@users.noreply.github.com>
2024-09-21 13:07:57 +0530

    frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import axios from "axios";
 
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
+// axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "https://wono-backend.vercel.app";
 
-axios.defaults.baseURL = "http://localhost:5000";
-
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <GoogleOAuthProvider clientId="358669748567-d4e1dl47ic6patb61sidq0ipdvllb0bn.apps.googleusercontent.com">
-    <App />
-    </GoogleOAuthProvider>
+      <GoogleOAuthProvider clientId="358669748567-d4e1dl47ic6patb61sidq0ipdvllb0bn.apps.googleusercontent.com">
+        <App />
+      </GoogleOAuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
