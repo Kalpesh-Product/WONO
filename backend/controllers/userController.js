9981a1ce3ccc04e695404ef6429dc650d21bb3a7
AiwinrajWono <wono.uiuxdev.intern03@gmail.com>
2024-09-24 19:22:04 +0530

    reg template and career page header

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -108,7 +108,11 @@ exports.registerUser = async (req, res) => {
             subject: "Welcome to Our Service!",
             html: `
         <h1>Welcome to Our Service, ${name}!</h1>
-        <p>Thank you for registering with us. We'll be contacting you within 24 hours</p>
+        <p>Thank you for registering with us. We'll be contacting you within 24 hours.</p>
+
+        <p>Email : ${email}</p>
+        <p>Password : ${password}</p>
+        
 
         <p>Feel free to explore our services and let us know if you need any assistance.</p>
         <p>Best Regards,<br>Wono</p>
