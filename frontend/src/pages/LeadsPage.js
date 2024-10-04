f092a59fbd9bc85dec20da89f35527303202bfd9
AllanMScode <97829785+AllanMScode@users.noreply.github.com>
2024-10-04 17:14:12 +0530

    leads page responsiveness

--- frontend/src/pages/LeadsPage.js
+++ frontend/src/pages/LeadsPage.js
@@ -863,7 +863,8 @@ const LeadsPage = () => {
         </div> */}
 
         <div className="last-section  w-100 fontPoppinsRegular">
-          <div className="pt-5 customMargin pb-5">
+          {/* <div className="pt-5 customMargin pb-5"> */}
+          <div className="bannerTopPadding customMargin bannerBottomPadding">
             <div className="grow-your-business-color w-100">
               <h2 className="m-0 fw-bold grow-your-business-text">
                 {/* <b className="leadsCtafontSize">
