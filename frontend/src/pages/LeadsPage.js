ea47f37e331a7a01a60ec4208ad059ad22008108
AllanMScode <97829785+AllanMScode@users.noreply.github.com>
2024-10-05 16:48:30 +0530

    responsiveness fix

--- frontend/src/pages/LeadsPage.js
+++ frontend/src/pages/LeadsPage.js
@@ -864,7 +864,7 @@ const LeadsPage = () => {
 
         <div className="last-section w-100 fontPoppinsRegular">
           {/* <div className="pt-5 customMargin pb-5"> */}
-          <div className="bannerTopPadding customMargin bannerBottomPadding">
+          <div className="bannerTopPadding customMargin bannerBottomPadding bannerPaddingSmall">
             <div className="grow-your-business-color w-100 ">
               <h2 className="m-0 fw-bold grow-your-business-text biggestText">
                 {/* <b className="leadsCtafontSize">
