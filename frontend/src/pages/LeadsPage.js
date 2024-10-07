30b24bb69e9f06798d43d77ee8fad3862b17eb5d
AllanMScode <97829785+AllanMScode@users.noreply.github.com>
2024-10-07 08:56:31 +0530

    no change

--- frontend/src/pages/LeadsPage.js
+++ frontend/src/pages/LeadsPage.js
@@ -518,7 +518,7 @@ const LeadsPage = () => {
         </div>
 
         <div className="marketing-section PPPP  fontPoppinsRegular  topPaddingForSections bottomPaddingForSections">
-          <div className="marketing-header  ">
+          <div className="marketing-header">
             {/* <div>
               <p className="mb-0">RUN & SCALE YOUR BUSINESS</p>
             </div> */}
