9fd61e0dd0ed0cabc605453fd55cfaafa33c453f
AllanMScode <97829785+AllanMScode@users.noreply.github.com>
2024-11-05 14:51:56 +0530

    changes on hold

--- frontend/src/components/ClientComponents/ClientHeader.jsx
+++ frontend/src/components/ClientComponents/ClientHeader.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+// import React from "react";
 
-const ClientHeader = () => {
-  return <div>ClientHeader</div>;
-};
+// const ClientHeader = () => {
+//   return (
+//     <nav
+//       className="navbar navbar-expand-lg bg-body-tertiary bg-dark"
+//       data-bs-theme="dark">
+//       <div className="container-fluid">
+//         <a className="navbar-brand" href="#">
+//           {/* Navbar */}
+//           <img src={biznestLogo} className="biznestLogoDimensions" />
+//         </a>
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           data-bs-toggle="collapse"
+//           data-bs-target="#navbarNav"
+//           aria-controls="navbarNav"
+//           aria-expanded="false"
+//           aria-label="Toggle navigation">
+//           <span className="navbar-toggler-icon" />
+//         </button>
+//         <div className="collapse navbar-collapse" id="navbarNav">
+//           <ul className="navbar-nav">
+//             <li className="nav-item">
+//               <Link
+//                 className="nav-link active"
+//                 aria-current="page"
+//                 to="/website">
+//                 Home
+//               </Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/website-login">
+//                 Login
+//               </Link>
+//             </li>
 
-export default ClientHeader;
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default ClientHeader;
--- frontend/src/pages/Client-Pages/ClientLandingPage.jsx
+++ frontend/src/pages/Client-Pages/ClientLandingPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import ClientHeader from "../../components/ClientComponents/ClientHeader";
 
 const ClientLandingPage = () => {
-  return <div>ClientLandingPage</div>;
+  return (
+    <>
+      {/* <ClientHeader/> */}
+      <div>ClientLandingPage</div>
+    </>
+  );
 };
 
 export default ClientLandingPage;
