66f7cbacda9cfd2ad173323764166de73f06529b
anushriinfipre <anushri.infipre@gmail.com>
2024-11-06 15:37:30 +0530

    Merge branch 'sankalp-dev' of https://github.com/Kalpesh-Product/WONO into anushri_dev

--- client-frontend/src/routes/Routes.jsx
+++ client-frontend/src/routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
+import Profile from "../pages/Profile";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,11 @@ const router = createBrowserRouter([
         index: true,
         element: <Home />,
       },
+      {
+        path:"/profile",
+        element:<Profile/>,
+      },
+      
     ],
   },
 ]);
