c5516657eb9ce4f5bce56a37cca801f28df99803
sankalp-wono <sankalp.wono@gmail.com>
2025-04-22 14:46:29 +0530

    fixed email sending

--- backend/email/nodemailerConfig.js
+++ backend/email/nodemailerConfig.js
@@ -1,25 +1,16 @@
 // config/nodemailerConfig.js
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 // Create and export the first transporter
-const aiwinMail = nodemailer.createTransport({
+const companyMail = nodemailer.createTransport({
   service: "gmail", // You can use other services like Yahoo, Outlook, etc.
   auth: {
-    user: "response@wono.co", // Your email
-    pass: "xmqw hyuz fdhh srtc", // Your email password or app password
-  },
-});
-
-// Create and export the second transporter
-const anushriMail = nodemailer.createTransport({
-  service: "gmail", // You can use other services like Yahoo, Outlook, etc.
-  auth: {
-    user: "anushri.bhagat263@gmail.com", // Your email
-    pass: "xwhn rrhx ldba vvfx", // Your email password or app password
+    user: process.env.RESPONSE_EMAIL, // Your email
+    pass: process.env.EMAIL_AIWIN_PASS
   },
 });
 
 module.exports = {
-    aiwinMail,
-    anushriMail
+  companyMail,
 };
